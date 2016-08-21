0fd06cb98427576043869fcbae12bbc5194c2255
xianing <1120814158@qq.com>
2016-08-21 15:57:28 +0800

    创建文件

---
+++ coverFlow显示图片/coverFlow显示图片/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  coverFlow显示图片
+//
+//  Created by user on 16/8/21.
+//  Copyright © 2016年 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ coverFlow显示图片/coverFlow显示图片/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  coverFlow显示图片
+//
+//  Created by user on 16/8/21.
+//  Copyright © 2016年 user. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
