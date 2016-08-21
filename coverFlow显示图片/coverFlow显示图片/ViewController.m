58b87ae5dfe20140ca37643fba021191c1a15555
xianing <1120814158@qq.com>
2016-08-21 16:27:08 +0800

    添加collectionView 加载数据

--- coverFlow显示图片/coverFlow显示图片/ViewController.m
+++ coverFlow显示图片/coverFlow显示图片/ViewController.m
@@ -8,20 +8,60 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+static NSString *cellID = @"cellID";
+
+@interface ViewController ()<UICollectionViewDataSource>
 
 @end
 
-@implementation ViewController
+@implementation ViewController {
+    
+    NSArray<UIImage *> *_imageList;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self setupUI];
+    
+    [self loadData];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+#pragma mark - UICollectionViewDataSource
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    
+    return _imageList.count;
 }
 
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:cellID forIndexPath:indexPath];
+    
+    cell.contentView.backgroundColor = [UIColor redColor];
+    
+    return cell;
+}
+
+- (void)setupUI {
+    
+    UICollectionViewFlowLayout *flowLayout = [[UICollectionViewFlowLayout alloc] init];
+    UICollectionView *collectionView = [[UICollectionView alloc] initWithFrame:CGRectMake(0, 100, self.view.bounds.size.width, 200) collectionViewLayout:flowLayout];
+    collectionView.backgroundColor = [UIColor lightGrayColor];
+    
+    collectionView.dataSource = self;
+    
+    [collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:cellID];
+    
+    [self.view addSubview:collectionView];
+}
+
+- (void)loadData {
+    
+    _imageList = @[
+                   [UIImage imageNamed:@"01"],
+                   [UIImage imageNamed:@"01"],
+                   [UIImage imageNamed:@"01"]
+                   ];
+    
+}
 @end
